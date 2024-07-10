5495ee962bf8eca5f452472068215a948472cff8
Kauan-Code19 <pkauprofissional@gmail.com>
2024-07-10 00:13:04 -0300

    projeto criado

---
+++ src/main/java/com/augefarma/controle_feira/ControleFeiraApplication.java
@@ -0,0 +1,13 @@
+package com.augefarma.controle_feira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFeiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/augefarma/controle_feira/ControleFeiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augefarma.controle_feira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFeiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
