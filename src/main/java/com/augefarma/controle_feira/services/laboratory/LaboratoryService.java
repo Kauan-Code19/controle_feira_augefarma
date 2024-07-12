b0854e523fb39b29432f3bd236aab36262bec17a
Kauan-Code19 <pkauprofissional@gmail.com>
2024-07-12 13:35:09 -0300

    criação do método getLaboratoryById com tratamento de exceções.

--- src/main/java/com/augefarma/controle_feira/services/laboratory/LaboratoryService.java
+++ src/main/java/com/augefarma/controle_feira/services/laboratory/LaboratoryService.java
@@ -3,7 +3,9 @@ package com.augefarma.controle_feira.services.laboratory;
 import com.augefarma.controle_feira.dtos.laboratory.LaboratoryDto;
 import com.augefarma.controle_feira.dtos.laboratory.LaboratoryResponseDto;
 import com.augefarma.controle_feira.entities.laboratory.LaboratoryEntity;
+import com.augefarma.controle_feira.exceptions.ResourceNotFoundException;
 import com.augefarma.controle_feira.repositories.laboratory.LaboratoryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,4 +39,26 @@ public class LaboratoryService {
 
         return new LaboratoryResponseDto(laboratory); // Convert the saved entity to a DTO and return it
     }
+
+    /**
+     * Service method to get a laboratory by its ID.
+     *
+     * @param laboratoryId the ID of the laboratory to retrieve
+     * @return a LaboratoryResponseDto containing the laboratory's information
+     * @throws ResourceNotFoundException if the laboratory is not found
+     */
+    @Transactional(readOnly = true)
+    public LaboratoryResponseDto getLaboratoryById(Long laboratoryId) {
+
+        try {
+            // Attempt to retrieve the laboratory entity from the repository using the given ID
+            LaboratoryEntity laboratory = laboratoryRepository.getReferenceById(laboratoryId);
+
+            return new LaboratoryResponseDto(laboratory); // If found, convert the entity to a DTO and return it
+        } catch (EntityNotFoundException exception) {
+            // If the laboratory entity is not found, throw a custom exception indicating the resource is not found
+            throw new ResourceNotFoundException("Resource not found");
+        }
+    }
+
 }
