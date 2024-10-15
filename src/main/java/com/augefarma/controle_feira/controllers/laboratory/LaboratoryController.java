387a401a006986accf5c822abc726174fbfdc88c
Kauan-Code19 <pkauprofissional@gmail.com>
2024-10-15 20:17:06 -0300

    implementação da feature delete para as entidades Laboratory, LaboratoryMember e PharmacyRepresentative

--- src/main/java/com/augefarma/controle_feira/controllers/laboratory/LaboratoryController.java
+++ src/main/java/com/augefarma/controle_feira/controllers/laboratory/LaboratoryController.java
@@ -6,9 +6,13 @@ import com.augefarma.controle_feira.services.laboratory.LaboratoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
@@ -45,5 +49,19 @@ public class LaboratoryController {
 
         // Return a ResponseEntity with status 201 (Created) and the response body
         return ResponseEntity.created(uri).body(laboratoryResponseDto);
+    }
+
+    @GetMapping("/search-by-corporateReason")
+    public ResponseEntity<LaboratoryResponseDto> getLaboratoryByCorporateReason(@RequestParam String corporateReason) {
+        LaboratoryResponseDto laboratoryResponseDto = laboratoryService.getLaboratoryByCorporateReason(corporateReason);
+
+        return ResponseEntity.ok(laboratoryResponseDto);
+    }
+
+    @DeleteMapping("/{laboratoryId}")
+    public ResponseEntity<Void> deleteLaboratory(@PathVariable Long laboratoryId) {
+        laboratoryService.deleteLaboratory(laboratoryId);
+
+        return ResponseEntity.noContent().build();
     }
 }
